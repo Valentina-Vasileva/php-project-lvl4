fbfe3d2cb9c7c110fd953db8b6edf66c286040c8
Ariss <valentina.ways@yandex.ru>
2021-04-23 17:00:51 +0300

    fix TaskStatusRequest

--- app/Http/Requests/StoreTaskStatusRequest.php
+++ app/Http/Requests/StoreTaskStatusRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use App\Models\TaskStatus;
 
 class StoreTaskStatusRequest extends FormRequest
 {
