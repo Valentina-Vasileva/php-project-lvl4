6b10a1a622c5ee05d4be778a5236c1ccc22dca47
Ariss <valentina.ways@yandex.ru>
2021-04-24 17:04:55 +0300

    fix task statuses view

--- resources/views/task_statuses/index.blade.php
+++ resources/views/task_statuses/index.blade.php
@@ -2,14 +2,16 @@
 
 @section('content')
 <div class="container">
+    <h1 class="mb-5">Статусы</h1>
     <div class="row">
-        <div class="col">
+        <div class="col my-2">
             <a href="{{ route('task_statuses.create') }}" class="btn btn-primary">Создать статус</a>
         </div>
     </div>
     <div class="row">
         <div class="col">
-                <table class="table table-responsive">
+            <div class="table-responsive">
+                <table class="table">
                     <tr>
                         <th scope="col">ID</th>
                         <th scope="col">Имя</th>
@@ -23,7 +25,7 @@
                                 <td scope="row"> {{ $status->name }} </td>
                                 <td>{{ $status->created_at }}</td>
                                 <td>
-                                    <a href="{{ route('task_statuses.destroy', ['task_status' => $status]) }}" data-method="delete" rel="nofollow" data-confirm="Вы уверены?">Удалить</a>
+                                    <a class="text-danger" href="{{ route('task_statuses.destroy', ['task_status' => $status]) }}" data-method="delete" rel="nofollow" data-confirm="Вы уверены?">Удалить</a>
                                     <a href="{{ route('task_statuses.edit', ['task_status' => $status]) }}">Изменить</a>
                                 </td>
                             </tr>
@@ -33,4 +35,10 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col">
+            {{ $taskStatuses->links() }}
+        </div>
+    </div>
+</div>
 @endsection
