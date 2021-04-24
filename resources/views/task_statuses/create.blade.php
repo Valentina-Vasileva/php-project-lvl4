44bdb7ddbec0ee83c4cc1b55e076c80e25196130
Ariss <valentina.ways@yandex.ru>
2021-04-24 17:22:10 +0300

    fix task statuses create view

--- resources/views/task_statuses/create.blade.php
+++ resources/views/task_statuses/create.blade.php
@@ -2,19 +2,28 @@
 
 @section('content')
 <div class="container">
-    <h1>Создать статус</h1>
+    <h1 class="mb-5">Создать статус</h1>
     <div class="row">
         <div class="col">
             {{Form::open(['url' => route('task_statuses.store')])}}
-                {{Form::text('name')}}
-                @if ($errors->any())
-                    <div class="invalis-feedback">
-                        @foreach ($errors->all() as $error)
-                            {{ $error }}
-                        @endforeach
-                    </div>
-                @endif
-                {{Form::submit('Создать')}}
+            {{Form::label('name', 'Имя')}}
+        </div>
+    </div>
+    <div class="row">
+        <div class="col col-4">
+            {{Form::text('name', '', array('class' => 'form-control'))}}
+            @if ($errors->any())
+                <div class="invalis-feedback">
+                    @foreach ($errors->all() as $error)
+                        {{ $error }}
+                    @endforeach
+                </div>
+            @endif
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            {{Form::submit('Создать', array('class' => 'btn btn-primary mt-3'))}}
             {{Form::close()}}
         </div>
     </div>
