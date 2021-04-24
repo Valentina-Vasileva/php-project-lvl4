229ffbb7da4542e643af456540fd16fa17f758b5
Ariss <valentina.ways@yandex.ru>
2021-04-24 17:56:46 +0300

    fix task statuses edit view

--- resources/views/task_statuses/edit.blade.php
+++ resources/views/task_statuses/edit.blade.php
@@ -2,21 +2,34 @@
 
 @section('content')
 <div class="container">
-    <h1>Изменение статуса</h1>
+    <h1 class="mb-5">Изменение статуса</h1>
     <div class="row">
         <div class="col">
             {{Form::model($taskStatus, ['url' => route('task_statuses.update', ['task_status' => $taskStatus]), 'method' => 'PATCH'])}}
-                {{Form::text('name')}}
-                @if ($errors->any())
-                    <div class="invalis-feedback">
-                        @foreach ($errors->all() as $error)
-                            {{ $error }}
-                        @endforeach
-                    </div>
-                @endif
-                {{Form::submit('Изменить')}}
-            {{Form::close()}}
+            <div class="form-row">
+                <div class="col">
+                    {{Form::label('name', 'Имя')}}
+                </div>
+            </div>            
+            <div class="form-row">
+                <div class="col-4">
+                    {{Form::text('name', $taskStatus->name, array('class' => 'form-control'))}}
+                    @if ($errors->any())
+                        <div class="invalis-feedback">
+                            @foreach ($errors->all() as $error)
+                                {{ $error }}
+                            @endforeach
+                        </div>
+                    @endif
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="col">             
+                    {{Form::submit('Изменить', array('class' => 'btn btn-primary mt-3'))}}
+                </div>
+            </div>
         </div>
+        {{Form::close()}}
     </div>
 </div>
 @endsection
