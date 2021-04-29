131f1f49177bc928739edaf201c4bd6e04f3082d
Ariss <valentina.ways@yandex.ru>
2021-04-29 20:08:22 +0300

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Seeders\TaskStatusSeeder;
+use Database\Seeders\TaskStatusSeeder;
 
 class DatabaseSeeder extends Seeder
 {
