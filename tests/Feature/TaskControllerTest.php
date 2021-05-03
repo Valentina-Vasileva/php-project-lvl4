5513bd2b062e2f721270235e6e6aef73b1823ea6
Ariss <valentina.ways@yandex.ru>
2021-05-03 18:09:43 +0300

    add tests for TaskController

--- tests/Feature/TaskControllerTest.php
+++ tests/Feature/TaskControllerTest.php
@@ -5,18 +5,118 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Task;
 
 class TaskControllerTest extends TestCase
 {
+    private $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+    }
     /**
-     * A basic feature test example.
+     * Test of tasks index.
      *
      * @return void
      */
-    public function test_example()
+    public function testIndex()
     {
-        $response = $this->get('/');
+        $response = $this->get(route('tasks.index'));
+        $response->assertOk();
+    }
 
-        $response->assertStatus(200);
+    /**
+     * Test of tasks show.
+     *
+     * @return void
+     */
+    public function testShow()
+    {
+        $task = Task::factory()->create();
+        $response = $this->get(route('tasks.show', ['task' => $task]));
+        $response->assertOk();
+    }
+
+    /**
+     * Test of tasks create.
+     *
+     * @return void
+     */
+    public function testCreate()
+    {
+        $response = $this->actingAs($this->user)
+            ->get(route('tasks.create'));
+        $response->assertOk();        
+    }
+
+    /**
+     * Test of tasks store.
+     *
+     * @return void
+     */
+    public function testStore()
+    {
+        $data = Task::factory()
+            ->make()
+            ->only(['name', 'description', 'status_id', 'created_by_id', 'assigned_to_id'])
+            ->all();
+        $response = $this->actingAs($this->user)
+            ->post(route('tasks.store'), $data);
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+
+        $this->assertDatabaseHas('tasks', $data);
+    }
+
+    /**
+     * Test of tasks edit.
+     *
+     * @return void
+     */
+    public function testEdit()
+    {
+        $task = Task::factory()->create();
+        $response = $this->actingAs($this->user)
+            ->get(route('tasks.edit', ['task' => $task]));
+        $response->assertOk(); 
+    }
+
+    /**
+     * Test of tasks update.
+     *
+     * @return void
+     */
+    public function testUpdate()
+    {
+        $task = Task::factory()->create();
+        $data = Task::factory()
+            ->make()
+            ->only(['name', 'description', 'status_id', 'created_by_id', 'assigned_to_id'])
+            ->all();
+        $response = $this->actingAs($this->user)
+            ->patch(route('tasks.update', ['task' => $task]), $data);
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+
+        $this->assertDatabaseHas('tasks', $data);
+    }
+
+    /**
+     * Test of tasks delete.
+     *
+     * @return void
+     */
+    public function testDelete()
+    {
+        $task = Task::factory()->create();
+        $response = $this->actingAs($this->user)
+            ->delete(route('tasks.destroy', ['task' => $task]));
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+
+        $this->assertDatabaseMissing('tasks', ['id' => $task->id]);
     }
 }
