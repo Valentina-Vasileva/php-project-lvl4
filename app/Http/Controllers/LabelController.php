c98a676a5d11672e3131ed37b465d44e9322e6bb
Ariss <valentina.ways@yandex.ru>
2021-05-17 17:42:39 +0300

    add labels controller

---
+++ app/Http/Controllers/LabelController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Label;
+
+class LabelController extends Controller
+{
+    public function __construct()
+    {
+        $this->authorizeResource(Label::class, 'label');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $labels = Label::orderBy('id', 'desc')->paginate();
+        return view('labels.index', compact('labels'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $label = new Label();
+        return view('labels.create', compact('label'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|unique:labels',
+            'description' => 'nullable|string'
+        ]);
+
+        $label = new Label();
+        $label->fill($data);
+        $label->save();
+        flash(__('Label has been added successfully'))->success();
+        return redirect()->route('labels.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Label  $label
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Label $label)
+    {
+        return view('labels.edit', compact('label'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Label  $label
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Label $label)
+    {
+        $data = $request->validate([
+            'name' => 'required|unique:labels,name,' . $label->id
+        ]);
+
+        $label->fill($data);
+        $label->save();
+        flash(__('Label has been updated successfully'))->success();
+        return redirect()->route('labels.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Label $label)
+    {
+        if ($label) {
+            if ($label->tasks()->exists()) {
+                flash(__('Failed to delete label'))->error();
+            } else {
+                $label->delete();
+                flash(__('Label has been deleted successfully'))->success();
+            }
+        }
+        return redirect()->route('labels.index');
+    }
+}
