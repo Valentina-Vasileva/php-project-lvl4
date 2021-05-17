27aa80249f6883965bd06d3cb2a7c2e205f25bb1
Ariss <valentina.ways@yandex.ru>
2021-05-17 17:42:18 +0300

    fix labels route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskStatusController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\Label;
 
 /*
 |--------------------------------------------------------------------------
