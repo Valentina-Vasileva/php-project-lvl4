d883b73a7cef9c754c3f15e3999beb03c5ad0960
Ariss <valentina.ways@yandex.ru>
2021-05-17 17:56:55 +0300

    add policies to authprovider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,9 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        'App\Models\Task' => 'App\Policies\TaskPolicy',
+        'App\Models\TaskStatus' => 'App\Policies\TaskStatusPolicy',
+        'App\Models\Label' => 'App\Policies\LabelPolicy'
     ];
 
     /**
