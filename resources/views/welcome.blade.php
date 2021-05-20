c5584a5ffcf0da7e08895063350df8d26f265526
Ariss <valentina.ways@yandex.ru>
2021-05-20 16:13:31 +0300

    add greeting to main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,14 @@
 @extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="jumbotron">
+        <h1 class="display-4">{{ __('Hello from Hexlet!') }}</h1>
+        <p class="lead">{{ __('Practical courses in programming') }}</p>
+        <hr class="my-4">
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="https://hexlet.io/" role="button">{{ __('Learn more') }}</a>
+        </p>
+    </div>
+</div>
+@endsection
